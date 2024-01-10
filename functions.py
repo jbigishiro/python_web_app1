6e746873038bbd0a807a79028a02bec6dce0399c
justin <justin.rvd@gmail.com>
2024-01-10 06:58:56 -0600

    add requirements.txt file

---
+++ functions.py
@@ -0,0 +1,18 @@
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list
+    of the to-do items
+    """
+    with open(filepath, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filename="todos.txt"):
+    """ Write the to-do items list in the text file."""
+    with open(filename, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello from the functions")
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    new_todo = st.session_state['new_todo'] + '\n'
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
+st.title('My Todo App')
+st.subheader('This is my todo app.')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=index)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[index]
+        st.rerun()
+
+st.text_input(label='New todo', placeholder='Enter a new todo',
+              on_change=add_todo, key='new_todo')
+
+st.session_state
